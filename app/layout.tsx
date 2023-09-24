604c76524a55ffb2672b439572c2de0403ae35f1
Matt Beiswenger <mattbeis@yahoo.com>
2023-09-24 14:41:56 -0500

    add github action

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +9,7 @@ export const metadata = {
   description: 'A simple URL shortener',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`bg-neutral-900 ${inter.className}`}>{children}</body>

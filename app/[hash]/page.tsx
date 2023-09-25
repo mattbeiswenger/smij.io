2a1eb27ce9b30319dc21f4cedb4e865354ea2445
Matt Beiswenger <mattbeis@yahoo.com>
2023-09-24 21:10:40 -0500

    fix typescript errors for #15

--- app/[hash]/page.tsx
+++ app/[hash]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 import { redis } from '../utils'
 
-export default async function Hash({ params }) {
+export default async function Hash({ params }: { params: { hash: string } }) {
   const hash = await redis.get<string>(params.hash)
   if (hash) {
     redirect(hash)

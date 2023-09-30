ae6d3fe78e02d46e2f9de0d0a770ddbeb269ac65
Matt Beiswenger <mattbeis@yahoo.com>
2023-09-30 14:04:32 -0500

    update styles to be more mobile friendly

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import * as elements from "typed-html";
 export function Layout({ children }: elements.Children) {
   return (
     <body class="bg-neutral-900">
-      <main class="container mx-auto flex min-h-screen flex-col items-center gap-16 px-10 py-40">
+      <main class="container mx-auto flex min-h-screen flex-col items-center gap-16 px-10 sm:py-40 py-24">
         {children}
       </main>
     </body>
